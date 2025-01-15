5e09f491e2ab8b5f5308f33b46d5c1633514f0fc
stuart taylor <github@j-notation.co.uk>
2025-01-15 09:41:02 +0000

    Update parameters.h
    implement get_parameter_address()

--- src/parameters/parameters.h
+++ src/parameters/parameters.h
@@ -4,57 +4,39 @@
 
 namespace pg1000 {
 
-// Parameter Groups
 enum class ParamGroup {
-    COMMON,
-    UPPER,
-    LOWER,
-    TOTAL_GROUPS
+    UPPER_PARTIAL_1,
+    UPPER_PARTIAL_2,
+    UPPER_COMMON,
+    LOWER_PARTIAL_1,
+    LOWER_PARTIAL_2,
+    LOWER_COMMON,
+    PATCH
 };
 
-// Parameter Types
 enum class ParamType {
-    CONTINUOUS_100,  // 0-100 range
-    CONTINUOUS_50,   // 0-50 range
-    SWITCH          // Binary or small range switches
+    WG,        // Wave Generator parameters
+    TVF,       // Time Variant Filter parameters
+    TVA,       // Time Variant Amplifier parameters
+    COMMON,    // Common parameters
+    PATCH      // Patch parameters
 };
 
-// Parameter definition
 struct Parameter {
-    uint16_t id;          // SysEx parameter ID
-    uint8_t value;        // Current value
-    uint8_t prev_value;   // Previous value
-    ParamType type;       // Parameter type
-    uint8_t max_value;    // Maximum value
-    uint8_t pot_number;   // Physical control number
-    ParamGroup group;     // Which parameter group
-    const char* name;     // Parameter name
-    bool active;          // Is this parameter currently active?
-    uint8_t cc_number;    // Optional CC mapping
+    const char* name;           // Parameter name
+    ParamGroup group;          // Which section this belongs to
+    ParamType type;           // Parameter type
+    union {
+        uint8_t partial_offset; // Offset for partial parameters (WG, TVF, TVA)
+        uint8_t common_offset;  // Offset for common parameters
+        uint8_t patch_offset;   // Offset for patch parameters
+    };
+    uint8_t value;            // Current value
+    uint8_t prev_value;       // Previous value
+    uint8_t min_value;        // Minimum allowed value
+    uint8_t max_value;        // Maximum allowed value
+    uint8_t pot_number;       // Which potentiometer controls this
+    bool active;             // Is this parameter currently active?
 };
-
-// Parameter state tracking
-struct ParameterState {
-    float current_value;  // Filtered current value
-    float alpha;          // Filter coefficient
-};
-
-// Get number of defined parameters
-int get_parameter_count();
-
-// Get parameter by index
-const Parameter* get_parameter(int index);
-
-// Get parameter by SysEx ID
-const Parameter* get_parameter_by_id(uint16_t id);
-
-// Get parameter by potentiometer number
-const Parameter* get_parameter_by_pot(uint8_t pot_number);
-
-// Update parameter value
-void update_parameter_value(const Parameter* param, uint8_t new_value);
-
-// Get filtered value for parameter
-float get_filtered_value(const Parameter* param);
 
 } // namespace pg1000
